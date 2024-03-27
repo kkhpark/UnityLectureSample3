6cce1e0ec1b9c9c039a10239771d2c9c1d09831f
kh park <pkgz888@gmail.com>
2024-03-27 15:27:52 +0900

    add gunshot using customized right controller trigger button

--- Assets/Script/OculusActionInput.cs
+++ Assets/Script/OculusActionInput.cs
@@ -11,22 +11,47 @@ public class OculusActionInput : MonoBehaviour
     //Action Asset
     public InputActionAsset InputActions;
     //Action Reference
-    public InputActionReference ActivateReference;
+    public InputActionReference PrimaryActionReference;
+
+    public InputActionReference SecondaryActionReference;
+
+    public InputActionReference TriggerActionReference;
+
+
+    //Fire Bullet
+    public GameObject Bullet;
+    public float bulletSpeed;
+
 
     private void OnEnable()
     {
         InputActions.Enable();
-        ActivateReference.action.performed += ActivateButton;
+        TriggerActionReference.action.performed += OnTriggerAction;
     }
 
-    private void ActivateButton(InputAction.CallbackContext callbackContext)
+    private void OnTriggerAction(InputAction.CallbackContext callbackContext)
     {
         Debug.Log("PrimaryButton");
+        FireBullet();
     }
 
     private void OnDisable()
     {
         InputActions.Disable();
-        ActivateReference.action.performed += ActivateButton;
+        TriggerActionReference.action.performed += OnTriggerAction;
+    }
+
+
+
+
+    // 총 쏘기
+    void FireBullet()
+    {
+        GameObject bullet = Instantiate(Bullet);
+        //컨트롤러에서 내가 바라보는 방향으로 총알이 나가야 함.
+        bullet.transform.position = transform.position; //지금 스크립트가 적용 되어 있는 GameObject(Controller)의 위치를 그대로 쓴다는거겠지.
+        bullet.transform.rotation = transform.rotation;
+        bullet.GetComponent<Rigidbody>().AddForce(transform.forward * bulletSpeed);
+
     }
 }
