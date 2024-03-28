76f109e357f3f1d9d931e50ede6e33f0b36a539a
kh park <pkgz888@gmail.com>
2024-03-28 13:54:26 +0900

    make monster follow player character

---
+++ Assets/Script/NavMeshMove.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshMove : MonoBehaviour
+{
+    NavMeshAgent nav;
+    public Transform targetPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nav = GetComponent<NavMeshAgent>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        nav.SetDestination(targetPos.position);
+    }
+}
